a88feff1127f5e1d4a3291a42b254b29b6e7ef6b
airgirl2305 <kheda.askhabova@gmail.com>
2022-05-01 19:26:18 +0300

    Мюню по выбору языка.  есть компонент, пока  всё топорно вставлено дивом на index.js

---
+++ src/components/LanguageMenu/LanguageMenu.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function LanguageMenu() {
+	return (<div className="language">
+	<img src="../../../public/images/lang/lang__ru.png" alt="ru" data-google-lang="ru" className="language__img"/>
+		<img src="../../../public/images/lang/lang__en.png" alt="en" data-google-lang="en" className="language__img"/>
+			<img src="../../../public/images/lang/lang__de.png" alt="de" data-google-lang="de" className="language__img"/>
+				<img src="../../../public/images/lang/lang__fr.png" alt="fr" data-google-lang="fr" className="language__img"/>
+						<img src="../../../public/images/lang/lang__es.png" alt="es" data-google-lang="es" className="language__img"/>
+							<img src="../../../public/images/lang/lang__it.png" alt="it" data-google-lang="it" className="language__img"/>
+								<img src="../../../public/images/lang/lang__zh.png" alt="zh" data-google-lang="zh-CN" className="language__img"/>
+
+</div>)
+}
+
+export default LanguageMenu
