c42e3f4f0a5903c9bc4c409e6ba0c84e06a0390c
airgirl2305 <kheda.askhabova@gmail.com>
2022-05-03 19:38:09 +0300

    Machine translation with a page reload.  The LanguageMenu component is created and not used. For now to see the effect of translation the index.js has an extra div tag.

--- src/redux/reducers/favoriteReducer.js
+++ src/redux/reducers/favoriteReducer.js
@@ -2,20 +2,21 @@ import initialState from '../initialState/initialState';
 import { ADD_TO_FAVORITE, DELETE_FROM_FAVORITE, EDIT_FAVORITE } from '../types/adsTypes';
 
 const favoriteReducer = ({ state = initialState, action }) => {
-  switch (action.type) {
+  const { type, payload } = action;
+  switch (type) {
     case ADD_TO_FAVORITE:
-      if (state.includes(action.payload)) {
+      if (state.includes(payload)) {
         return state;
       }
-      return [...state, action.payload];
+      return [...state, payload];
 
     case DELETE_FROM_FAVORITE:
-      return state.filter((el) => el.id !== action.payload);
+      return state.filter((el) => el.id !== payload);
 
     case EDIT_FAVORITE:
       return state.map((el) => {
-        if (el.id === action.payload.id) {
-          return action.payload;
+        if (el.id === payload.id) {
+          return payload;
         }
         return el;
       });
--- src/redux/reducers/loaderReducer.js
+++ src/redux/reducers/loaderReducer.js
@@ -1,7 +1,6 @@
 import { DISABLE_LOADER, ENABLE_LOADER } from '../types/loaderTypes';
 
-// eslint-disable-next-line default-param-last
-const loaderReducer = (state = false, action) => {
+const loaderReducer = ({ state = false, action }) => {
   switch (action.type) {
     case ENABLE_LOADER:
       return true;
