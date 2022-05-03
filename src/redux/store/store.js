e5d91b8601e253126e2209111eca0d1f9cd44e8a
airgirl2305 <kheda.askhabova@gmail.com>
2022-05-03 20:31:35 +0300

    updated store with sagas

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,13 +1,18 @@
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import initialState from '../initialState/initialState';
+import createSagaMiddleware from 'redux-saga';
 import rootReducer from '../reducers/rootReducer';
+import rootSaga from '../sagas/rootSaga';
+import initialState from '../initialState/initialState';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   rootReducer,
   initialState,
-  composeWithDevTools(applyMiddleware(thunk)),
+  composeWithDevTools(applyMiddleware(thunk, sagaMiddleware)),
 );
 
+sagaMiddleware.run(rootSaga);
 export default store;
