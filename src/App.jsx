c10dd653c85d51af0b3d0f2dfc3915dba5ee7ef1
airgirl2305 <kheda.askhabova@gmail.com>
2022-05-09 22:12:15 +0300

    imported maps - working

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,53 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { Route, Routes } from 'react-router-dom'
 
-import PrivateRoute from './components/PrivateRouter/PrivateRouter';
-import UserDetail from './components/UserDetail/UserDetail';
-import UserEdit from './components/UserEdit/UserEdit';
-import Header from './components/Header/Header';
-import SignUp from './components/Forms/SignUp/SignUp';
-import SignIn from './components/Forms/SignIn/SignIn';
-import { checkAuth } from './redux/actions/userAction';
-import AddAdvertisement from './components/Forms/AddAdvertisement/AddAdvertisement';
-import Main from './pages/Main/Main';
-import Footer from './components/Footer/Footer';
+import PrivateRoute from './components/PrivateRouter/PrivateRouter'
+import UserDetail from './components/UserDetail/UserDetail'
+import UserEdit from './components/UserEdit/UserEdit'
+import Header from './components/Header/Header'
+import SignUp from './components/Forms/SignUp/SignUp'
+import SignIn from './components/Forms/SignIn/SignIn'
+import { checkAuth } from './redux/actions/userAction'
+import AddAdvertisement from './components/Forms/AddAdvertisement/AddAdvertisement'
+import Main from './pages/Main/Main'
+import Footer from './components/Footer/Footer'
 
-import Advertisements from './pages/Advertisements/Advertisements';
-import { AboutHelpAdoption } from './pages/AboutHelpAdoption/AboutHelpAdoption';
-import { AllPetsCare } from './pages/AllPetsCare/AllPetsCare';
-import { CatCare } from './pages/CatCare/CatCare';
-import { DogCare } from './pages/DogCare/DogCare';
-import { HelpingPets } from './pages/HelpingPets/HelpingPets';
-import { Shelters } from './pages/Shelters/Shelters';
-import { DogBreeds } from './pages/DogBreeds/DogBreeds';
-import { CatBreeds } from './pages/CatBreeds/CatBreeds';
-import { Chat } from './pages/Chat/Chat';
-import { UserProfile } from './pages/UserProfile/UserProfile';
-import { PetsTips } from './pages/PetsTips/PetsTips';
-import DescriptAdvert from './pages/DescriptAdvert/DescriptAdvert';
+import Advertisements from './pages/Advertisements/Advertisements'
+import { AboutHelpAdoption } from './pages/AboutHelpAdoption/AboutHelpAdoption'
+import { AllPetsCare } from './pages/AllPetsCare/AllPetsCare'
+import { CatCare } from './pages/CatCare/CatCare'
+import { DogCare } from './pages/DogCare/DogCare'
+import { HelpingPets } from './pages/HelpingPets/HelpingPets'
+import { Shelters } from './pages/Shelters/Shelters'
+import { DogBreeds } from './pages/DogBreeds/DogBreeds'
+import { CatBreeds } from './pages/CatBreeds/CatBreeds'
+import { Chat } from './pages/Chat/Chat'
+import { UserProfile } from './pages/UserProfile/UserProfile'
+import { PetsTips } from './pages/PetsTips/PetsTips'
+import DescriptAdvert from './pages/DescriptAdvert/DescriptAdvert'
 
-import './App.sass';
+import './App.sass'
+import MapPage from './pages/MapPage/MapPage'
 
-function App() {
-  const dispatch = useDispatch();
-
+function App () {
+  const dispatch = useDispatch()
+  
   useEffect(() => {
-    dispatch(checkAuth());
-  }, []);
-
+    dispatch(checkAuth())
+  }, [])
+  
   return (
     <>
-      <Header />
+      <Header/>
       <div className="container">
         <Routes>
-          <Route path="/" element={<Main />} />
+          <Route path="/" element={<Main/>}/>
           <Route
             path="/users/:id"
             element={
               <PrivateRoute>
-                <UserDetail />
+                <UserDetail/>
               </PrivateRoute>
             }
           />
@@ -54,31 +55,32 @@ function App() {
             path="/user/edit"
             element={
               <PrivateRoute>
-                <UserEdit />
+                <UserEdit/>
               </PrivateRoute>
             }
           />
-          <Route path="/auth/signup" element={<SignUp />} />
-          <Route path="/auth/signin" element={<SignIn />} />
-          <Route path="/addAdvert" element={<AddAdvertisement />} />
-          <Route path="/advertisements" element={<Advertisements />} />
-          <Route path="/about-help-adoption" element={<AboutHelpAdoption />} />
-          <Route path="/all-pets-care" element={<AllPetsCare />} />
-          <Route path="/cat-care" element={<CatCare />} />
-          <Route path="/dog-care" element={<DogCare />} />
-          <Route path="/helping-pets" element={<HelpingPets />} />
-          <Route path="/shelters" element={<Shelters />} />
-          <Route path="/dog-breeds" element={<DogBreeds />} />
-          <Route path="/cat-breeds" element={<CatBreeds />} />
-          <Route path="/chat" element={<Chat />} />
-          <Route path="/userprofile" element={<UserProfile />} />
-          <Route path="/tips" element={<PetsTips />} />
-          <Route path="/posts/:id" element={<DescriptAdvert />} />
+          <Route path="/auth/signup" element={<SignUp/>}/>
+          <Route path="/auth/signin" element={<SignIn/>}/>
+          <Route path="/addAdvert" element={<AddAdvertisement/>}/>
+          <Route path="/advertisements" element={<Advertisements/>}/>
+          <Route path="/about-help-adoption" element={<AboutHelpAdoption/>}/>
+          <Route path="/all-pets-care" element={<AllPetsCare/>}/>
+          <Route path="/cat-care" element={<CatCare/>}/>
+          <Route path="/dog-care" element={<DogCare/>}/>
+          <Route path="/helping-pets" element={<HelpingPets/>}/>
+          <Route path="/shelters" element={<Shelters/>}/>
+          <Route path="/dog-breeds" element={<DogBreeds/>}/>
+          <Route path="/cat-breeds" element={<CatBreeds/>}/>
+          <Route path="/chat" element={<Chat/>}/>
+          <Route path="/userprofile" element={<UserProfile/>}/>
+          <Route path="/tips" element={<PetsTips/>}/>
+          <Route path="/posts/:id" element={<DescriptAdvert/>}/>
+          <Route path="/map" element={<MapPage/>}/>
         </Routes>
       </div>
-      <Footer />
+      <Footer/>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/pages/DescriptAdvert/DescriptAdvert.jsx
+++ src/pages/DescriptAdvert/DescriptAdvert.jsx
@@ -1,38 +1,39 @@
-import { useEffect, useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { getOneAdvertThunk } from '../../redux/thunks/getOneAdvertThunk';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination } from 'swiper';
-import * as endPoints from '../../config/endPoints';
-import './DescriptAdvert.sass';
-import { Chat } from '../Chat/Chat';
+import { useEffect, useState } from 'react'
+import { NavLink, useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { getOneAdvertThunk } from '../../redux/thunks/getOneAdvertThunk'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination } from 'swiper'
+import * as endPoints from '../../config/endPoints'
+import './DescriptAdvert.sass'
+import { Chat } from '../Chat/Chat'
+import AdMap from '../../components/AdMap/AdMap'
 
 const DescriptAdvert = () => {
   const [positionChat, setPositionChat] = useState('chat__disable')
-
-  let params = useParams();
-  const dispatch = useDispatch();
-
+  
+  let params = useParams()
+  const dispatch = useDispatch()
+  
   useEffect(() => {
-    dispatch(getOneAdvertThunk(params.id));
-  }, []);
-  const dataAdvert = useSelector((state) => state.getOneAdvert);
-
+    dispatch(getOneAdvertThunk(params.id))
+  }, [])
+  const dataAdvert = useSelector((state) => state.getOneAdvert)
+  
   const styleChat = () => {
     if (positionChat === 'chat__disable') {
-      setPositionChat('chat__enable');
+      setPositionChat('chat__enable')
     } else {
-      setPositionChat('chat__disable');
+      setPositionChat('chat__disable')
     }
   }
-
+  
   return (
+    
     <div className="descript">
-
+      
       <Chat style={positionChat} changeStyle={styleChat}/>
-
+      
       <div className="advert">
         <div className="advert__title title">
           <div className="title__navigate">
@@ -63,9 +64,9 @@ const DescriptAdvert = () => {
               {dataAdvert.images?.map((img, index) => {
                 return (
                   <SwiperSlide key={'keyAdvert' + index}>
-                    <img src={endPoints.getImagePet(img)} alt="img" />
+                    <img src={endPoints.getImagePet(img)} alt="img"/>
                   </SwiperSlide>
-                );
+                )
               })}
             </Swiper>
           </div>
@@ -94,8 +95,10 @@ const DescriptAdvert = () => {
           </ul>
         </div>
       </div>
+      
+      <AdMap/>
     </div>
-  );
-};
+  )
+}
 
-export default DescriptAdvert;
+export default DescriptAdvert
